82d8182dfd10c1a35f6122b2eaaab55251c9b13d
federicomanni97 <manni_federico@libero.it>
2023-11-13 15:38:06 +0100

    cards api taked + method axios

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <main class="container">
+      <div class="row">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { store } from './data/store'
+import axios from 'axios'
+export default {
+  data() {
+    
+
+    return {
+      store
+    }
+  },
+  methods: {
+      getCards(){
+        const url = store.apiUrl;
+        axios.get(url).then((response) =>{
+          console.log(response.data);
+          store.cardList = response.data;
+        })
+      }
+    },
+    created(){
+      this.getCards()
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
