2f1b564e461d61f9a8b39d88cdea81ac43c6de70
federicomanni97 <manni_federico@libero.it>
2023-11-14 17:21:00 +0100

    image quality fixed

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
           <p class="text-light w-100 py-4 bg-dark">Found {{ store.cardList.length }} cards</p>
           <CardComponent
             v-for="(element, index) in store.cardList"
-            :imageCard="element.card_images[0].image_url_small" 
+            :imageCard="element.card_images[0].image_url" 
             :nameCard="element.name" 
             :archetypeCard="element.archetype" 
           />
--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <select v-model='selectAll' @change="$emit('selectElement', selectAll)" class="p-2" name="" id="">
+        <select v-model='selectAll' @change="$emit('selectElement', selectAll)" class="p-2 my-4" name="" id="">
             <option value="">All</option>
             <option value="alien">Alien</option>
             <option value="crystal beast">Crystal Beast</option>
             <option value="elemental hero">Elemental HERO</option>
-            <option value="infenoble arms">Infernoble Arms</option>
+            <option value="egyptian god">Egyptian God</option>
             <option value="noble knight">Noble Knight</option>
         </select>
     </div>
